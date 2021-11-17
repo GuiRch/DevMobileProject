7af14fddbff38912b7cbb9dfb323859374cd4e6b
bjeabby1507 <BJeabby@hotmail.fr>
2021-11-17 19:38:55 +0100

    ::construction: fragment update

--- app/src/main/java/com/guillaume/devmobileproject/fragmentHome.kt
+++ app/src/main/java/com/guillaume/devmobileproject/fragmentHome.kt
@@ -5,6 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.appcompat.widget.AppCompatImageButton
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -29,6 +34,16 @@ class fragmentHome : Fragment() {
         }
     }
 
+    // navigate to fragment
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val navController = view.findNavController()
+
+        val player = view.findViewById<AppCompatImageView>(R.id.imageButton4)
+        player.setOnClickListener {
+            navController.navigate(R.id.action_fragmentHome_to_playerFragment)
+        }
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
